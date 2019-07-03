2bcddd834f69d56e139525fd0debd6dce17dbbc0
Kaitian Xie <xkaitian@gmail.com>
2019-07-03 11:15:09 -0700

    Add redux devtools

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -7,12 +7,15 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import routes from '../both/routes';
 import mainReducer from '../../api/redux/reducers';
+import { createLogger } from 'redux-logger'; // eslint-disable-line
+import { composeWithDevTools } from 'redux-devtools-extension'; // eslint-disable-line
 
 const preloadedState = window.__PRELOADED_STATE__; // eslint-disable-line
 
 delete window.__PRELOADED_STATE__; // eslint-disable-line
 
-const store = createStore(mainReducer, preloadedState, applyMiddleware(thunk));
+// eslint-disable-next-line
+const store = createStore(mainReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, createLogger())));
 
 const App = () => (
   <Provider store={store}>
